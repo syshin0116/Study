4ea13e4b15e4d54686bc8f41a71a0aa21b3f7c08
syshin0116 <syshin0116@gmail.com>
2022-10-21 15:16:28 +0900

    ignore

---
+++ Spring Boot/myhome/src/main/java/com/godcoder/myhome/model/Role.java
@@ -0,0 +1,25 @@
+package com.godcoder.myhome.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+}
