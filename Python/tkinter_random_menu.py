98bbdb795c5ec2a8f2d79ebcdcf160ea43b05748
syshin0116 <syshin0116@gmail.com>
2022-12-23 15:58:57 +0900

    testproject

--- Python/tkinter_random_menu.py
+++ Python/tkinter_random_menu.py
@@ -4,7 +4,8 @@ import os
 
 class MainWindow():
     def __init__(self, root):
-        # list of imgs located in each categorical directories
+
+        # list of images located in each categorical directories
         self.img_korean = [PhotoImage(file='./food/korean/'+i) for i in os.listdir('./food/korean')]
         self.img_western = [PhotoImage(file='./food/western/'+i) for i in os.listdir('./food/western')]
         self.img_chinese = [PhotoImage(file='./food/chinese/'+i) for i in os.listdir('./food/chinese')]
@@ -14,7 +15,6 @@ class MainWindow():
         # config for tk window
         root.title('메뉴추천기')
         root.config(padx=10, pady=10, bg='lightblue')
-
 
         # canvas
         self.canvas = Canvas(root, height=680, width=680, bg='ivory')
@@ -26,15 +26,15 @@ class MainWindow():
         self.canvas.create_text(340, 60, text='오늘 뭐 먹지?', fill='brown', font=('나눔바른펜', 30, 'bold'))
 
         # buttons
-        btn1 = Button(root, text='한식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_korean, 'korean'))
+        btn1 = Button(root, text='한식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_korean))
         btn1.pack()
-        btn2 = Button(root, text='양식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_western, 'western'))
+        btn2 = Button(root, text='양식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_western))
         btn2.pack()
-        btn3 = Button(root, text='중식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_chinese, 'chinese'))
+        btn3 = Button(root, text='중식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_chinese))
         btn3.pack()
-        btn4 = Button(root, text='일식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_japanese, 'japanese'))
+        btn4 = Button(root, text='일식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_japanese))
         btn4.pack()
-        btn5 = Button(root, text='분식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_bunsik, 'bunsik'))
+        btn5 = Button(root, text='분식', padx=20, font=('나눔바른펜', 20, 'bold'), command=lambda: self.update_img(self.img_bunsik))
         btn5.pack()
 
         btn1.place(x=20, y=600)
@@ -44,7 +44,7 @@ class MainWindow():
         btn5.place(x=540, y=600)
 
     # update img on click
-    def update_img(self, img_list, type):
+    def update_img(self, img_list):
         img = random.choice(img_list)
         self.canvas.itemconfig(self.img_on_canvas, image=img)
 
