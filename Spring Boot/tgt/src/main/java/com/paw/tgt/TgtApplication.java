78409e7ac3b2f8105cae8b0be50d714670fe4cf7
syshin0116 <syshin0116@gmail.com>
2023-01-17 03:13:40 +0900

    spring boot

---
+++ Spring Boot/tgt/src/main/java/com/paw/tgt/TgtApplication.java
@@ -0,0 +1,13 @@
+package com.paw.tgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgtApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/tgt/src/test/java/com/paw/tgt/TgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paw.tgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
