8b3556a1db655feb4096126eb9b6d4ba8ddef8dc
syshin0116 <syshin0116@gmail.com>
2023-01-30 02:48:37 +0900

    spring security config

--- Spring Boot/tgt/src/main/java/com/paw/tgt/config/SecurityConfig.java
+++ Spring Boot/tgt/src/main/java/com/paw/tgt/config/SecurityConfig.java
@@ -8,22 +8,47 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
+                .requestMatchers("/").permitAll()
                 .and()
-                .httpBasic();
+                .authorizeHttpRequests().requestMatchers("/board/**").authenticated()
+                .and().formLogin();
+                
 //        http.formLogin();
 //        http.httpBasic();
         return http.build();
     }
 
+    //Indian Youtube Authentication
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails admin= User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user= User.withUsername("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
