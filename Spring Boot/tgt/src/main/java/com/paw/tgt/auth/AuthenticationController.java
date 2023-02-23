ccabdcce2c0317c8942120ba358bfb61d5b5bcd9
syshin0116 <syshin0116@gmail.com>
2023-02-23 21:01:47 +0900

    jwt login, register

--- Spring Boot/tgt/src/main/java/com/paw/tgt/auth/AuthenticationController.java
+++ Spring Boot/tgt/src/main/java/com/paw/tgt/auth/AuthenticationController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -15,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/register")
+    @PostMapping ("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        System.out.print("AuthenticationController>>"+request);
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
