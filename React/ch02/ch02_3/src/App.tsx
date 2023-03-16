3e5d62a085c41968b7ca2b2c59f4c2eaeb004fa9
syshin0116 <syshin0116@gmail.com>
2023-03-17 03:32:54 +0900

    react chap2_3

--- React/ch02/ch02_3/src/App.tsx
+++ React/ch02/ch02_3/src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default class App extends Component {
+//   render(){
+//     const isLoading = true
+//     if (isLoading) return <p>loading...</p>
+//     return (
+//       <ul>
+//         <li>
+//           <a href='http://www.google.com'>
+//             <p>go to Google</p>
+//           </a>
+//         </li>
+//       </ul>
+//     )
+//   }
+// }
+
+// export default class App extends Component {
+//   render(){
+//     const isLoading = true
+//     const children = (
+//       <li>
+//         <a href='http://www.google.com'>
+//           <p>go to Google</p>
+//         </a>
+//       </li>
+//     )
+//     return(
+//       <div>
+//         {isLoading && <p>loading...</p>}
+//         {!isLoading && <ul>{children}</ul>}
+//       </div>
+//     )
+//   }
+// }
+
+// export default class App extends Component{
+//   render(){
+//       const isLoading = true
+//       const children = isLoading ? (
+//         <p>loading...</p>
+//       ) : (
+//         <ul>
+//           <li>
+//             <a href='http://www.google.com'>
+//               <p>go to Google</p>
+//             </a>
+//           </li>
+//         </ul>
+//       )
+//       return <div>{children}</div>
+//   }
+// }
+
+import ClassComponent from './ClassComponent'
+
+export default class App extends Component{
+  render(){
+    return(
+      <ul>
+        <ClassComponent />
+        <ClassComponent />
+      </ul>
+    )
+  }
 }
-
-export default App;
