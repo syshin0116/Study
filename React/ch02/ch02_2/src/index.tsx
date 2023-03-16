df4aba4947e9a057e83e2db776638866cc208822
syshin0116 <syshin0116@gmail.com>
2023-03-16 19:33:59 +0900

    react chap2_3

--- React/ch02/ch02_2/src/index.tsx
+++ React/ch02/ch02_2/src/index.tsx
@@ -1,5 +1,6 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as D from './data'
 
 // const CE = React.createElement
 
@@ -45,8 +46,22 @@ import ReactDOM from 'react-dom/client';
 // const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 // root.render(rootVirtualDOM)
 
-const children = [0, 1, 2].map((n: number)=> <h3>Hello world! {n}</h3>)
+// const children = [0, 1, 2].map((n: number)=> <h3>Hello world! {n}</h3>)
+// const rootVirtualDOM = <div>{children}</div>
+
+// const root = ReactDOM. createRoot (document.getElementById('root') as HTMLElement)
+// root.render (rootVirtualDOM)
+
+const children = D.makeArray(3).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src = {D.randomAvatar()} width={100} height={100} />
+  </div>
+))
 const rootVirtualDOM = <div>{children}</div>
 
-const root = ReactDOM. createRoot (document.getElementById('root') as HTMLElement)
-root.render (rootVirtualDOM)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(rootVirtualDOM)
---
+++ React/ch02/ch02_3/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: false, 
+    isxBracketSameLine: true, 
+    singleQuote: true,
+    trailingComma: 'none', 
+    arrowParens: 'avoid',
+    semi: false,
+    printWidth: 90
+}
