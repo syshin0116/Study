7beacb48a6fb152d1219c6f3cba69a0a31a27bb5
syshin0116 <syshin0116@gmail.com>
2023-03-21 01:58:12 +0900

    vscode push

---
+++ React/ch02/ch02_4/src/App.tsx
@@ -0,0 +1,7 @@
+import P from "./P"
+
+export default function App(){
+  const texts = ['hello', 'world'].map((text, index) => 
+    <P key={index} children={text}/>)
+  return <div children={texts}/>
+}
