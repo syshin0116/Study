1ac00db54997ca1d85bcc64319aa27f3b404f918
syshin0116 <syshin0116@gmail.com>
2023-04-01 11:06:50 +0900

    react EventBubbling

--- React/ch02/ch02_5/src/pages/EventBubbling.tsx
+++ React/ch02/ch02_5/src/pages/EventBubbling.tsx
@@ -1,3 +1,18 @@
-export default function CopyMe(){
-    return <div>CopyMe</div>
+import { ECDH } from "crypto"
+import type { SyntheticEvent } from "react"
+export default function EventBubbling(){
+    const onDivClick = (e: SyntheticEvent)=>{
+        const {isTrusted, target, bubbles, currentTarget} = e
+        console.log('click evnet bubles on <div>', isTrusted, target, bubbles, currentTarget)
+    }
+    const onButtonClick = (e:SyntheticEvent)=>{
+        const {isTrusted, target, bubbles} = e
+        console.log('click event starts at <button>', isTrusted, target, bubbles)
+    }
+    return (
+    <div onClick={onDivClick}>
+        <p>EvnetBubbling</p>
+        <button onClick={onButtonClick}>Click Me</button>
+    </div>
+    )
 }
