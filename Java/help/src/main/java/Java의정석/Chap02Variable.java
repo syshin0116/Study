8e9cb02f7081490f39a8284fd6459f6ea8fd2afc
syshin0116 <syshin0116@gmail.com>
2023-08-26 13:41:46 +0900

    programmers 택시 합승

---
+++ Java/help/src/main/java/Java의정석/Chap02Variable.java
@@ -0,0 +1,15 @@
+package Java의정석;
+
+public class Chap02Variable {
+    public static void main(String[] args) {
+        int $ystem = 1;
+        int channel#5 = 1;
+        int 7eleven = 1;
+        int If = 1;
+        int 자바 = 1;
+        int new = 1;
+        int $MAX_NUM = 1;
+        int hellp@com = 1;
+
+    }
+}
